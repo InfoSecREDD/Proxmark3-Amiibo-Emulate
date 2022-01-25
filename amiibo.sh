65813f7eb7123c21abc27c2d4116627b278c3e3d
REDD <ar1k88gamingllc@gmail.com>
2022-01-25 11:28:42 -0800

    Fixing to new method.
    No need for EML format. Proxmark3 can emulate Amiibos bin files.

--- amiibo.sh
+++ amiibo.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
 #
-#  Title: Amiibo Convert & Emulate Bash Script for Proxmark3
+#  Title: Amiibo Emulate Bash Script for Proxmark3
 #  Author: InfoSecREDD
 #
-#  SYNTAX: amiibo.sh <amiibo_bin_file> <output_eml_file>
-#          amiibo.sh <amiibo_eml_file>
+#  SYNTAX: bash amiibo.sh <amiibo_bin_file>
+#
 
 
 AMIIBO="${1}"
-EML_FILE="${2}"
 
 DUMP_DIR="${HOME}/dumps"
 PROXMARK_DIR="${HOME}/proxmark3"
 
 CLIENT="${PROXMARK_DIR}/client/proxmark3"
-CONVERT="${PROXMARK_DIR}/tools/pm3_amii_bin2eml.pl"
 
 USE_TCP_BRIDGE="1"
 
 PORT="4321"
 TCP="tcp:localhost:${PORT}"
 
-if [ ! -f "${AMIIBO}" ]; then
+if ([ ! -f "${AMIIBO}" ]); then
         echo "There is no file by that name. Try again."
         exit 1
 fi
-
-if [[ "${AMIIBO}" == *.eml ]]; then
-        TEMPVAR1="${DUMP_DIR}/${AMIIBO}"
-        EML_FILE="${AMIIBO}"
-        TEMPVAR2="${TEMPVAR1}"
-        AMIIBO="${TEMPVAR2}"
-        echo "Using supplied Amiibo eml file."
-elif [[ ! -f "${DUMP_DIR}/${EML_FILE}" ]]; then
-        ${CONVERT} "${AMIIBO}" > "${DUMP_DIR}/${EML_FILE}"
+if [ "${AMIIBO: -4}" == ".bin" ]; then
+        echo "Valid bin file found."
+        EMULATE_FILE="${DUMP_DIR}/amiibo.bin"
+        if [ -f "${EMULATE_FILE}" ]; then
+                rm -rf "${EMULATE_FILE}"
+        fi
+        cp -rf "${AMIIBO}" "${EMULATE_FILE}"
 else
-        echo "Converted Amiibo file found."
+        echo "Error: Please provide a valid file to emulate."
 fi
 if [ "${USE_TCP_BRIDGE}" -eq "1" ]; then
         ${CLIENT} ${TCP} -c "hf mf eclr"
-        ${CLIENT} ${TCP} -c "hf mfu eload -f ${DUMP_DIR}/${EML_FILE} --ul"
+        ${CLIENT} ${TCP} -c "hf mfu eload -f ${EMULATE_FILE} --ul"
         ${CLIENT} ${TCP} -c "hf 14a sim -t 7"
 elif [ "${USE_TCP_BRIDGE}" -eq "0" ]; then
         ${CLIENT} -c "hf mf eclr"
-        ${CLIENT} -c "hf mfu eload -f ${DUMP_DIR}/${EML_FILE} --ul"
+        ${CLIENT} -c "hf mfu eload -f ${EMULATE_FILE} --ul"
         ${CLIENT} -c "hf 14a sim -t 7"
 else
         echo "Error: Set correct value for Variables."
 fi
+if [ -f "${EMULATE_FILE}" ]; then                                         echo "Cleaning up after emulation."
+        rm -rf "${EMULATE_FILE}"
+fi
+echo "Done."
+exit 0
