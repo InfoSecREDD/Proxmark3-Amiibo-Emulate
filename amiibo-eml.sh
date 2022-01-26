5f521f64a7e285d3a322f0e99c082438db3f9da0
REDD <ar1k88gamingllc@gmail.com>
2022-01-25 16:32:18 -0800

    Update amiibo-eml.sh

--- amiibo-eml.sh
+++ amiibo-eml.sh
@@ -3,12 +3,10 @@
 #  Title: Amiibo Convert & Emulate Bash Script for Proxmark3
 #  Author: InfoSecREDD
 #
-#  SYNTAX: amiibo-eml.sh <amiibo_bin_file> <output_eml_file>
-#          amiibo-eml.sh <amiibo_eml_file>
+#  SYNTAX: bash amiibo.sh </path/to/bin/file.bin>
 
 
 AMIIBO="${1}"
-EML_FILE="${2}"
 
 DUMP_DIR="${HOME}/dumps/amiibo_eml"
 PROXMARK_DIR="${HOME}/proxmark3"
@@ -29,26 +27,31 @@ if [ ! -f "${AMIIBO}" ]; then
         exit 1
 fi
 
-if [ "${AMIIBO: -4}" == ".eml" ]; then
-        TEMPVAR1="${DUMP_DIR}/${AMIIBO}"
-        EML_FILE="${AMIIBO}"
-        AMIIBO="${TEMPVAR1}"
-        echo "Using supplied Amiibo eml file."
-elif [ ! -f "${DUMP_DIR}/${EML_FILE}" ]; then
-        ${CONVERT} "${AMIIBO}" > "${DUMP_DIR}/${EML_FILE}"
-else
-        echo "Converted Amiibo file found."
+if [ "${AMIIBO: -4}" == ".bin" ]; then
+        EMULATE_FILE="${DUMP_DIR}/amiibo.eml"
+        AMIIBO_TEMP="${DUMP_DIR}/amiibo.bin"
+        if [ -f "${EMULATE_FILE}" ]; then
+                rm -rf "${EMULATE_FILE}"
+        fi
+        cp -rf "${AMIIBO}" "${AMIIBO_COPY}"
+        ${CONVERT} "${AMIIBO_COPY}" > "${EMULATE_FILE}"
+        if [ -f "${AMIIBO_COPY}" ]; then
+                rm -rf "${AMIIBO_COPY}"
+        fi
 fi
 if [ "${USE_TCP_BRIDGE}" -eq "1" ]; then
         ${CLIENT} ${TCP} -c "hf mf eclr"
-        ${CLIENT} ${TCP} -c "hf mfu eload -f ${DUMP_DIR}/${EML_FILE} --ul"
+        ${CLIENT} ${TCP} -c "hf mfu eload -f ${EMULATE_FILE} --ul"
         ${CLIENT} ${TCP} -c "hf 14a sim -t 7"
 elif [ "${USE_TCP_BRIDGE}" -eq "0" ]; then
         ${CLIENT} -c "hf mf eclr"
-        ${CLIENT} -c "hf mfu eload -f ${DUMP_DIR}/${EML_FILE} --ul"
+        ${CLIENT} -c "hf mfu eload -f ${EMULATE_FILE} --ul"
         ${CLIENT} -c "hf 14a sim -t 7"
 else
         echo "Error: Set correct value for Variables."
 fi
-echo "Done.
-exit 0
+if [ -f "${EMULATE_FILE}" ]; then
+        echo "Cleaning up after emulation."
+        rm -rf "${EMULATE_FILE}"
+fi
+echo "Done."
